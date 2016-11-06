b3319d4aa7a2ececed6c2fb39c601d1e8a6041e4
Maksym Planeta <mcsim.planeta@gmail.com>
2016-11-06 10:47:39 +0100

    Fix bugs in taurus and taurus-mini

--- scripts/conf/taurus.py
+++ scripts/conf/taurus.py
@@ -44,6 +44,7 @@ class Taurus(manager.Machine):
         self.env = os.environ.copy()
         self.env['OMP_NUM_THREADS'] = '1'
         self.env['INTERFERENCE_LOCALID'] = 'MV2_COMM_WORLD_LOCAL_RANK'
+        self.env['INTERFERENCE_HACK'] = 'true'
 
         self.prefix = 'INTERFERENCE'
 
--- scripts/conf/taurus_mini.py
+++ scripts/conf/taurus_mini.py
@@ -13,15 +13,15 @@ class Taurus_Mini(manager.Machine):
         base =  self.env['HOME'] + "/interference-bench/miniapps/"
 
         self.modules_load = 'source {}/mini.env'.format(base)
-        compile_command = self.modules_load + '; {}; make'
+        compile_command = self.modules_load + '; cd ../src-mpi ; make'
         tmpl = './{prog} {size_param}'
         self.group = \
             manager.BenchGroup(Miniapp, prog = ("CoMD-mpi",),
-                               size_param = ("-i 2 -j 1 -k 1",),
-                               size = (2,),
-                               np = (2,),
-                               wd = base + "CoMD/src-mpi/",
-                               compile_command = compile_command.format('cd src-mpi'),
+                               size_param = ("-i 3 -j 2 -k 4",),
+                               size = (24,),
+                               np = (24,),
+                               wd = base + "CoMD/bin",
+                               compile_command = compile_command,
                                tmpl = tmpl)#  + \
             # manager.BenchGroup(Miniapp, prog = ("CoMD-ampi",),
             #                    size_param = ("-i 2 -j 2 -k 1",),
@@ -36,21 +36,25 @@ class Taurus_Mini(manager.Machine):
             #                    wd = base + "CoMD-1.1/bin/",
             #                    tmpl = tmpl)
 
-        # self.group = \
-        #             manager.BenchGroup(Miniapp, prog = ("lassen_mpi",),
-        #                        size_param = ("default 2 2 2 200 200 200",),
-        #                        size = (8,),
-        #                        np = (1, 2),
-        #                        wd = base + "Lassen-1.0/",
-        #                        tmpl = tmpl)
+        compile_command = self.modules_load + '; make lassen_mpi'
+        self.group += \
+                    manager.BenchGroup(Miniapp, prog = ("lassen_mpi",),
+                               size_param = ("default 2 2 2 200 200 200",),
+                               size = (8,),
+                               np = (8),
+                               compile_command = compile_command,
+                               wd = base + "lassen/",
+                               tmpl = tmpl)
 
-        # self.group = \
-        #     manager.BenchGroup(Miniapp, prog = ("lulesh2.0",),
-        #                size_param = ("-i 300 -c 10 -b 3",),
-        #                size = (8,),
-        #                np = (2,),
-        #                wd = base + "Lulesh-2.0/",
-        #                tmpl = tmpl)
+        compile_command = self.modules_load + '; make'
+        self.group += \
+            manager.BenchGroup(Miniapp, prog = ("lulesh2.0",),
+                       size_param = ("-i 300 -c 10 -b 3",),
+                       size = (8,),
+                       np = (8,),
+                       wd = base + "lulesh2.0.3/",
+                       compile_command = compile_command,
+                       tmpl = tmpl)
 
         self.mpiexec = 'mpirun_rsh'
         self.mpiexec_np = '-np'
@@ -64,11 +68,12 @@ class Taurus_Mini(manager.Machine):
 
         self.env['OMP_NUM_THREADS'] = '1'
         self.env['INTERFERENCE_LOCALID'] = 'MV2_COMM_WORLD_LOCAL_RANK'
+        self.env['INTERFERENCE_HACK'] = 'true'
 
         self.prefix = 'INTERFERENCE'
 
         self.schedulers = ("cfs",)
-        self.affinities = ("2-3", "1,3")
+        self.affinities = ("12-23",)
 
         self.nodes = (1,)
 
@@ -93,7 +98,7 @@ class Taurus_Mini(manager.Machine):
                                self.mpiexec_np, str(context.bench.np),
                                '-ssh',
                                '-export-all'])
-        command = "{} ; taskset 0xFFFFFFFF {} {} {} ./../bin/{}"
+        command = "{} ; taskset 0xFFFFFFFF {} {} {} ./{}"
         return command.format(self.modules_load, self.mpiexec, parameters,
                               self.preload.format(self.get_lib()), context.bench.name)
 
