7e2bf4729e0caf1cde5001f348696b6d93ca5232
Maksym Planeta <mcsim.planeta@gmail.com>
2016-11-30 13:55:27 +0100

    Update planeta-os machine file

--- scripts/conf/planetaos.py
+++ scripts/conf/planetaos.py
@@ -5,40 +5,81 @@ import manager
 
 from .npb import Npb
 
+import math
 
 class PlanetaOS(manager.Machine):
     def __init__(self, args):
-        base = "/home/desertfox/research/projects/ffmk/interference-bench/"
+        self.env = os.environ.copy()
+        base = self.env['HOME'] + "/research/projects/ffmk/measurements/interference-bench/"
+
+        cpu_per_node = 4
+
+        def np_square(nodes, oversub):
+            np = nodes * oversub * cpu_per_node
+            return math.floor(math.sqrt(np))**2
+
+        def np_power2(nodes, oversub):
+            return nodes * oversub * cpu_per_node
+
+        def np_func(nodes, oversub):
+            return nodes * oversub * cpu_per_node
+
+        def compile_command(wd, prog, nodes, oversub, size):
+            # A HACK
+            if prog in ("bt", "sp"):
+                np = np_square(nodes, oversub)
+            elif prog in ("is", "cg",):
+                np = np_power2(nodes, oversub)
+            else:
+                np = np_func(nodes, oversub)
+            # HACK END
+
+            return 'cd {} ;' \
+                ' make {} NPROCS={} CLASS={}'.format(wd, prog, np, size)
 
         common_params = {
             'nodes': (1,),
-            'schedulers': ("cfs", "pinned"),
+            'schedulers': ("cfs", "pinned_cyclic"),
             'affinity': ("2-3", "1,3"),
+            'oversub': (1, 2, 4),
+            'compile_command': compile_command,
+            'size': ('W', 'S',),
+            'distribution' : 'blocked',
         }
+
+        mz_params = {
+            'wd': base + "/NPB3.3.1-MZ/NPB3.3-MZ-MPI/"
+        }
+
+        npb_params = {
+            'wd': base + "/NPB3.3.1/NPB3.3-MPI/"
+        }
+
         self.group = \
-            manager.BenchGroup(Npb, prog=("bt-mz", "sp-mz"),
-                               size=("S",),
-                               np=(2, 4),
-                               **common_params,
-                               wd=base + "NPB3.3.1-MZ/NPB3.3-MZ-MPI/") + \
-            manager.BenchGroup(Npb, prog=("bt-mz", "sp-mz"),
-                               size=("W",),
-                               np=(2, 4, 8),
-                               **common_params,
-                               wd=base + "NPB3.3.1-MZ/NPB3.3-MZ-MPI/") + \
-            manager.BenchGroup(Npb,
-                               prog=("bt", "sp"),
-                               size=("W", "S"),
-                               np=(4, 9),
-                               **common_params,
-                               wd=base + "/NPB3.3.1/NPB3.3-MPI/") + \
-            manager.BenchGroup(Npb,
-                               prog=("cg", "ep", "ft",
-                                     "is", "lu", "mg"),
-                               size=("W", "S"),
-                               np=(2, 4, 8),
-                               **common_params,
-                               wd=base + "/NPB3.3.1/NPB3.3-MPI/")
+            manager.BenchGroup(Npb, **common_params, **mz_params,
+                               np=np_func,
+                               prog=("bt-mz", "sp-mz"))
+
+
+        self.group += \
+            manager.BenchGroup(Npb, **common_params, **npb_params,
+                               np=np_func,
+                               prog=("ep", "lu", "mg"))
+
+        self.group += \
+            manager.BenchGroup(Npb, **common_params, **npb_params,
+                               np=np_power2,
+                               prog=("is", "cg",))
+
+        self.group += \
+            manager.BenchGroup(Npb, **common_params, **npb_params,
+                               np=np_power2,
+                               prog=("ft",))
+
+        self.group += \
+            manager.BenchGroup(Npb, **common_params, **npb_params,
+                               np=np_square,
+                               prog=("bt", "sp"))
 
         self.mpiexec = 'mpirun'
         self.mpiexec_np = '-np'
@@ -48,8 +89,8 @@ class PlanetaOS(manager.Machine):
 
         self.lib = manager.Lib('openmpi')
 
-        self.env = os.environ.copy()
         self.env['INTERFERENCE_LOCALID'] = 'OMPI_COMM_WORLD_LOCAL_RANK'
+        self.env['INTERFERENCE_LOCAL_SIZE'] = 'OMPI_COMM_WORLD_LOCAL_SIZE'
 
         self.prefix = 'INTERFERENCE'
 
@@ -60,13 +101,13 @@ class PlanetaOS(manager.Machine):
         self.nodelist = self.get_nodelist()
         self.hostfile_dir = self.env['HOME'] + '/hostfiles'
 
-        super(PlanetaOS, self).__init__(args)
+        super().__init__(args)
 
     def get_nodelist(self):
         return [socket.gethostname()]
 
     def format_command(self, context):
-        parameters = " ".join([self.mpiexec_hostfile.format(self.hostfile.path),
+        parameters = " ".join([self.mpiexec_hostfile.format(context.hostfile.path),
                                self.mpiexec_np, str(context.bench.np),
                                self.preload.format(self.get_lib()),
                                '-oversubscribe',
